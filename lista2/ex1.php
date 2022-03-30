42908d699dc8aaa4f40c524d78689e9c1db083d8
Maria <lima.maria@aluno.ifsp.edu.br>
2022-03-30 11:24:05 -0300

    exerc

--- lista2/ex1.php
+++ lista2/ex1.php
@@ -1,7 +1,11 @@
 <?php
 $n_par=0;
-$soma;
-for($n=0;$n<100;$n++){
+$soma=0;
+for($n_par=0;$n_par<100;$n_par++){
+   if ($n_par%2==0){
     $soma=$soma+$n_par;
+
+   }
 }
+echo "A soma de todos os números pares menores que 100 é: " .$soma;
 ?>
