ae7ab04dc958adc4503f3b55472a6c1d3b8744cc
Maria <lima.maria@aluno.ifsp.edu.br>
2022-03-30 11:32:22 -0300

    exerc

--- lista2/ex2.php
+++ lista2/ex2.php
@@ -1,16 +1,16 @@
 <?php
-$op=2;
+$op=11;
 switch($op){
     case 1: echo "JANEIRO"; break;
     case 2: echo "FEVEREIRO";break;
     case 3: echo "MARÇO"; break;
     case 4: echo "ABRIL"; break;
-    case 5: "MAIO"; break;
-    case 6: "JUNHO"; break;
-    case 7: "JULHO"; break;
-    case 8: "AGOSTO"; break;
-    case 9: "SETEMBRO"; break;
-    case 10: "OUTUBRO"; break;
-    case 11: "NOVEMBRO"; break;
-    case 12: "DEZEMBRO"; break;
+    case 5: echo "MAIO"; break;
+    case 6: echo "JUNHO"; break;
+    case 7: echo "JULHO"; break;
+    case 8: echo "AGOSTO"; break;
+    case 9: echo "SETEMBRO"; break;
+    case 10: echo "OUTUBRO"; break;
+    case 11: echo "NOVEMBRO"; break;
+    case 12: echo "DEZEMBRO"; break;
 }
