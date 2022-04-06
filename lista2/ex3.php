cf56e9638d073c9fed4c2b9ce552a6e433e7bb0d
Maria <lima.maria@aluno.ifsp.edu.br>
2022-04-06 09:40:11 -0300

    exercício_primo

--- lista2/ex3.php
+++ lista2/ex3.php
@@ -1,7 +1,18 @@
 <?php
-$n_primo=0;
-for($n_primo;$n_primo>0 and $n_primo<200;$n_primo++){
-    if($n_primo){
 
+for($num=1;$num<=200;$num++){
+    $n_primo=1;
+    for($divisor=2; $divisor<$num; $divisor++){
+
+        if($num%$divisor==0){
+            $n_primo=0;
+            break;
+        }
+    }
+    if($n_primo){
+        echo "O número " .$num. " é primo" ."<br>";
+    }
+    else{
+        echo "O número " .$num. " não é primo" ."<br>";
     }
 }
