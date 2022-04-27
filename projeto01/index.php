1501b21d6df7056265b710bd96a1ff682b0d81cd
Maria <lima.maria@aluno.ifsp.edu.br>
2022-04-27 10:42:01 -0300

    menu e lista

---
+++ projeto01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+</head>
+<body>
+    <h1>Menu principal</h1>
+    <div id="menu">
+    <ul>
+        <li><a href="cadastro_usuario.htm">Cadastro Usuário</a></li>
+        <li><a href="listar_usuarios.php">Listar Usuários</a></li>
+    </ul>
+</div>
+</body>
+</html>
