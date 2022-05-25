a971e4395efeafe3b195b1568590091939311151
Maria <lima.maria@aluno.ifsp.edu.br>
2022-05-25 12:15:27 -0300

    exercício do caralho

---
+++ projeto02/altera_usuario.php
@@ -0,0 +1,39 @@
+<?php
+    include('conexao.php');
+    $id_usuario = $_GET ['id_usuario'];
+    $sql = 'SELECT *FROM usuario where id_usuario='.$id_usuario;
+    $result = mysqli_query($con, $sql);
+    $row = mysqli_fetch_array($result);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<style>
+    body{
+        background-color: plum;
+    }
+</style>
+</head>
+
+<body>
+    <h1> Alteração de ususário</h1>
+    <?php echo "<img class='center' src='data:image/jpeg;base64,"
+    .base64_encode ($row["foto_blob"])."'width='150' height='150'/>";?>
+    <form action="altera_usuario_exe.php" method="post" enctype="multipart/form-data">
+        Nome: <input name="nome" value="<?php echo $row['nome_usuario']?>" size= "50" type="text"><br>
+        E-mail: <input name="email" value="<?php echo $row['email_usuario']?>" size= "50" type="text"><br>
+        Telefone: <input name="tel" value="<?php echo $row['telefone_usuario']?>" size= "30" type="text"><br>
+        <input type="file" id="foto" name="foto" accept="image/*"/>
+        <input name="id_usuario" type="hidden" value="<?php echo $row['id_usuario']?>">
+        <br><br><input id="btnEnviar" type="submit" value="Enviar">
+    </form>
+    <div>
+
+    </div>
+</body>
+</html>
---
+++ projeto02/cadastro_usuario.php
@@ -0,0 +1,29 @@
+<?php
+include('conexao.php'); 
+$fotoNome = $_FILES['foto']['name'];
+$target_dir = "upload/";
+$target_file = $target_dir . basename($_FILES["foto"]["name"]);
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+$extensions_arr = array("jpg","jpeg","png","gif");
+
+
+if( in_array($imageFileType,$extensions_arr) ){        
+    if(move_uploaded_file($_FILES['foto']['tmp_name'],$target_dir.$fotoNome)){
+        $fotoBlob = addslashes(file_get_contents($target_dir.$fotoNome));
+    }
+}
+$nome_usuario = $_POST['nome'];
+$email_usuario = $_POST['email'];
+$fone_usuario = $_POST['tel'];
+echo "<P> Nome do usuário: ".$nome_usuario."<br>";
+echo "<P> E-mail: ".$email_usuario."<br>";
+echo "<P> Telefone: ".$fone_usuario."<br>";
+$sql = "INSERT INTO usuario (nome_usuario, email_usuario, telefone_usuario, foto_blob, foto_nome)
+        VALUES ('".$nome_usuario."','".$email_usuario."','".$fone_usuario."','".$fotoBlob."','".$fotoNome."')";
+
+$result = mysqli_query($con, $sql);
+if($result)
+    echo "Dados inseridos com sucesso";
+else
+    echo "Erro ao inserir no banco de dados: ".mysqli_error($con);
+?>
